bc9f311c2298b36ca2221e467072268bf0e03712
Ralf Lang <lang@b1-systems.de>
2015-12-16 11:25:30 +0100

    Use the dynamic view

--- index.php
+++ index.php
@@ -10,4 +10,20 @@
  * @author Ralf Lang <lang@b1-systems.de>
  */
 
+/* Determine View */
+/* For the purpose of demonstration, we will just force dynamic view instead
+   Remove the commented code if you only have dynamic view.
+   Activate the commented code if you need to support traditional view */
+
+//if ($GLOBALS['registry']->getView() == Horde_Registry::VIEW_DYNAMIC) {
+    require_once __DIR__ . '/lib/Application.php';
+    Horde_Registry::appInit('boneyard');
+    $injector->getInstance('Boneyard_Ajax')->init();
+    require BONEYARD_TEMPLATES . '/dynamic/index.inc';
+    echo $injector->getInstance('Boneyard_View_Sidebar');
+    $page_output->footer();
+    exit;
+//}
+
+// traditional view
 require __DIR__ . '/list.php';
