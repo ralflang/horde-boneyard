7577f96191af3101f01ec0dac5bba024ef4d7a7a
Ralf Lang <lang@b1-systems.de>
2015-12-16 12:11:09 +0100

    Add dynamic view templates: The main structure index.inc, the sidebar template and two bogus example pages

---
+++ templates/dynamic/sidebar.html.php
@@ -0,0 +1,17 @@
+<div class="boneyardMenu">
+  <div id="boneyardNavExample1" class="horde-subnavi">
+    <div class="horde-subnavi-icon horde-data">
+      <a class="icon"></a>
+    </div>
+    <div class="horde-subnavi-point"><?php echo _("Example 1 Page") ?></div>
+  </div>
+  <div class="horde-sidebar-split"></div>
+  <div id="boneyardNavExample2" class="horde-subnavi">
+    <div class="horde-subnavi-icon horde-data">
+      <a class="icon"></a>
+    </div>
+    <div class="horde-subnavi-point"><?php echo _("Example 2 Page") ?></div>
+  </div>
+</div>
+<div class="horde-sidebar-split"></div>
+
